e24dd63ddb6212a8cffacbaa5ada994b2880b138
AshrithaJayanthi <ashrithajayanthi11@gmail.com>
2021-10-29 14:33:13 +0530

    home , schemes , register created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SchemesComponent } from './schemes/schemes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+ { path:'home' , component:HomeComponent},
+ {path:'schemes',component:SchemesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SchemesComponent } from './schemes/schemes.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SchemesComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
